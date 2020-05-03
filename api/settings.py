889049d71ea0e12f529be739699ee950627c8abc
Bruno Camargo <brunocam1195@gmail.com>
2020-05-03 12:32:12 -0300

    Change api folder's name

--- api/settings.py
+++ api/settings.py
@@ -32,7 +32,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'xpenses_backend.urls'
+ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
     {
@@ -50,7 +50,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'xpenses_backend.wsgi.application'
+WSGI_APPLICATION = 'api.wsgi.application'
 
 
 # Database
